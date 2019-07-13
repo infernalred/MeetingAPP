6815479c3eaca5019e58995d5f73cabaf398acd6
infernalred <39046593+infernalred@users.noreply.github.com>
2019-07-13 21:51:39 +0300

    Initial commit

---
+++ MeetingAPI/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingAPI.Models
+{
+    public class Room
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string EventColor { get; set; }
+    }
+}
---
+++ MeetingAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingAPI.Models
+{
+    public class User
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
