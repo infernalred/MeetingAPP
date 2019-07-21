a829d4fa7def4c6df39974ff88dad3effdd4a41b
infernalred <39046593+infernalred@users.noreply.github.com>
2019-07-21 13:50:05 +0300

    before calendar

--- MeetingFront/MeetingFront/ClientApp/src/app/home/home.component.ts
+++ MeetingFront/MeetingFront/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Calendar } from '@fullcalendar/core';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent  {
+  calendarPlugins = [resourceTimeGridPlugin];
 }
+
